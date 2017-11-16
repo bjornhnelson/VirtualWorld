578808d1fecc7838c0a4806c9ee2512cf8e35048
Bjorn Nelson <bjornhnelson@gmail.com>
2017-11-15 23:26:31 -0800

    updated

--- VirtualWorld/src/AnimatedSchedule.java
+++ VirtualWorld/src/AnimatedSchedule.java
@@ -19,24 +19,4 @@ public abstract class AnimatedSchedule extends AnimatedObjects {
                 getAnimationPeriod());
     }
 
-    protected Point nextPosition(WorldModel world, Point destPos)
-    {
-        int horiz = Integer.signum(destPos.x - getPosition().x);
-        Point newPos = new Point(getPosition().x + horiz,
-                getPosition().y);
-
-        if (horiz == 0 || world.isOccupied(newPos))
-        {
-            int vert = Integer.signum(destPos.y - getPosition().y);
-            newPos = new Point(getPosition().x, getPosition().y + vert);
-
-            if (vert == 0 || world.isOccupied(newPos))
-            {
-                newPos = getPosition();
-            }
-        }
-
-        return newPos;
-    }
-
 }
