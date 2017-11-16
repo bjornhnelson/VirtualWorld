455def07aa9fb15f1c63c4bb471e46e55584f388
Bjorn Nelson <bjornhnelson@gmail.com>
2017-11-15 23:54:21 -0800

    updated

--- VirtualWorld/src/AnimatedObjects.java
+++ VirtualWorld/src/AnimatedObjects.java
@@ -1,9 +1,5 @@
 import java.util.List;
 import processing.core.PImage;
-import java.util.function.Predicate;
-import java.util.function.BiPredicate;
-import java.util.stream.Stream;
-import java.util.function.Function;
 
 public abstract class AnimatedObjects extends DynamicObjects implements Animated {
 
@@ -25,41 +21,5 @@ public abstract class AnimatedObjects extends DynamicObjects implements Animated
         int index = (getImageIndex() + 1) % getImages().size();
         setImageIndex(index);
     }
-
-    protected Point nextPosition(WorldModel world, Point destPos) {
-        SingleStepPathingStrategy pathStrategy = new SingleStepPathingStrategy();
-
-        Predicate<Point> canPassThrough = p -> world.isOccupied(p);
-        BiPredicate<Point, Point> withinReach = (p1, p2) -> p1.adjacent(p2);
-        Function<Point, Stream<Point>> potentialNeighbors = PathingStrategy.CARDINAL_NEIGHBORS;
-
-        List<Point> pathStrategyList = pathStrategy.computePath(getPosition(), destPos, canPassThrough, withinReach, potentialNeighbors);
-
-        if (pathStrategyList.size() > 0) {
-            return pathStrategyList.get(0);
-        }
-        else {
-            return getPosition();
-        }
-    }
-
-            /*
-        int horiz = Integer.signum(destPos.x - getPosition().x);
-        Point newPos = new Point(getPosition().x + horiz,
-                getPosition().y);
-
-        if (horiz == 0 || world.isOccupied(newPos))
-        {
-            int vert = Integer.signum(destPos.y - getPosition().y);
-            newPos = new Point(getPosition().x, getPosition().y + vert);
-
-            if (vert == 0 || world.isOccupied(newPos))
-            {
-                newPos = getPosition();
-            }
-        }
-
-        return newPos;
-        */
 
 }
--- VirtualWorld/src/AnimatedSchedule.java
+++ VirtualWorld/src/AnimatedSchedule.java
@@ -1,6 +1,10 @@
 import java.util.List;
 import processing.core.PImage;
 import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.function.BiPredicate;
+import java.util.stream.Stream;
+import java.util.function.Function;
 
 public abstract class AnimatedSchedule extends AnimatedObjects {
 
@@ -18,5 +22,46 @@ public abstract class AnimatedSchedule extends AnimatedObjects {
         scheduler.scheduleEvent(this, AnimationAction.createAnimationAction(this, 0),
                 getAnimationPeriod());
     }
+
+
+    protected Point nextPosition(WorldModel world, Point destPos) {
+        SingleStepPathingStrategy pathStrategy = new SingleStepPathingStrategy();
+
+        Predicate<Point> canPassThrough = p -> world.isOccupied(p);
+        BiPredicate<Point, Point> withinReach = (p1, p2) -> p1.adjacent(p2);
+        Function<Point, Stream<Point>> potentialNeighbors = PathingStrategy.CARDINAL_NEIGHBORS;
+
+        List<Point> pathStrategyList = pathStrategy.computePath(getPosition(), destPos, canPassThrough, withinReach, potentialNeighbors);
+
+        if (pathStrategyList.size() > 0) {
+            return pathStrategyList.get(0);
+        }
+        else {
+            return getPosition();
+        }
+    }
+
+
+    /*
+    protected Point nextPosition(WorldModel world, Point destPos)
+    {
+        int horiz = Integer.signum(destPos.x - getPosition().x);
+        Point newPos = new Point(getPosition().x + horiz,
+                getPosition().y);
+
+        if (horiz == 0 || world.isOccupied(newPos))
+        {
+            int vert = Integer.signum(destPos.y - getPosition().y);
+            newPos = new Point(getPosition().x, getPosition().y + vert);
+
+            if (vert == 0 || world.isOccupied(newPos))
+            {
+                newPos = getPosition();
+            }
+        }
+
+        return newPos;
+    }
+    */
 
 }
